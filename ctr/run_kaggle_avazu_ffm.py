0519b29805ece4387471a16f2aa4e560ceb383b5
breakhearts <lxx1234@gmail.com>
2016-04-03 17:59:29 +0800

    =

--- ctr/run_kaggle_avazu_ffm.py
+++ ctr/run_kaggle_avazu_ffm.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         eta, _lambda, k, iter = sys.argv[1:5]
         eta, _lambda, k, iter = float(eta), float(_lambda), int(k), int(iter)
+        feature_map = utility.HashFeatureMap.load(feature_map_file)
     else:
         eta, _lambda, k, iter = 0.03, 0.00002, 4, 15
     if len(sys.argv) > 5 and sys.argv[5] == "dummy":
@@ -17,7 +18,8 @@ if __name__ == "__main__":
         feature_map_file = "feature_map_v3_dummy.json"
         train_data_file = "app_train_features_v3_dummy.csv"
         test_data_file = "app_test_features_v3_dummy.csv"
-    train_fs = utility.FeatureStream(utility.get_date_file_path(feature_map_file), utility.get_date_file_path(train_data_file))
+        feature_map = utility.DummyFeatureMap.load(feature_map_file)
+    train_fs = utility.FeatureStream(feature_map, utility.get_date_file_path(train_data_file))
     #model_file = "app_model_ffm_4_id_{0}_{1}.json".format(eta, _lambda)
     print "training......"
     alg = ffm.FFM(train_fs.feature_count(), train_fs.field_count(), k)
@@ -29,7 +31,7 @@ if __name__ == "__main__":
         log_loss = alg.train(train_fs, _lambda, eta, report_interval=-1)
         #alg.dump_model(utility.get_date_file_path(model_file))
         print "testing......"
-        test_fs = utility.FeatureStream(utility.get_date_file_path(feature_map_file), utility.get_date_file_path(test_data_file))
+        test_fs = utility.FeatureStream(feature_map, utility.get_date_file_path(test_data_file))
         alg.test(test_fs, report_interval=-1)
         if pre_logloss > 0 and abs(float(log_loss - pre_logloss)) / pre_logloss < 0.001:
             break
