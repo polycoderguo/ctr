e2eda397d52612e12f329952a65e30f2ca2a6567
breakhearts <lxx1234@gmail.com>
2016-05-28 14:10:13 +0800

    =

---
+++ ctr/algorithm/lr.py
@@ -0,0 +1,45 @@
+from __future__ import absolute_import
+import numpy as np
+from ctr.common import utility
+import cPickle
+
+
+class LR(object):
+    def __init__(self, total_features):
+        self.total_features = total_features
+        self.W = np.zeros(total_features + 1)
+
+    def train(self, feature_stream, _lambda, eta, init=False, report_interval=1000000):
+        if init:
+            self.W = np.zeros(self.total_features + 1)
+        for count, (_, click, features) in enumerate(feature_stream):
+            t = self.W[0]
+            for feature_index in features:
+                t += self.W[feature_index]
+            p = utility.sigmoid(t)
+            g = eta * (p - click)
+            validate_helper = utility.ValidateHelper(report_interval=report_interval)
+            validate_helper.out_put()
+            self.W[0] -= g
+            for feature_index in features:
+                self.W[feature_index] -= (g + _lambda * self.W[feature_index])
+            validate_helper.update(p, click, 0.5)
+        validate_helper.out_put()
+
+    def test(self, feature_stream, p_threshold=0.5, report_interval=1000000):
+        validate_helper = utility.ValidateHelper(report_interval=report_interval)
+        for count, (_, click, features) in enumerate(feature_stream):
+            t = self.W[0]
+            for feature_index in features:
+                t += self.W[feature_index]
+            p = utility.sigmoid(t)
+            validate_helper.update(p, click, p_threshold)
+        validate_helper.out_put()
+
+    def dump_model(self, filename):
+        with open(filename, "wb") as f:
+            cPickle.dump(self.__dict__, f)
+
+    def load_model(self, filename):
+        with open(filename, "rb") as f:
+            self.__dict__ = cPickle.load(f)
---
+++ ctr/run_kaggle_avazu_lr.py
@@ -0,0 +1,27 @@
+from __future__ import absolute_import
+from ctr.algorithm import lr
+from ctr.common import utility
+import sys
+
+if __name__ == "__main__":
+    feature_map_file = "app_feature_map.json"
+    train_data_file = "app_train_features.csv"
+    test_data_file = "app_test_features.csv"
+    if len(sys.argv) > 2:
+        eta, _lambda, iter = sys.argv[1:]
+        eta, _lambda, iter = float(eta), int(iter)
+    else:
+        eta, _lambda, iter = 0.01, 0, 20
+    train_fs = utility.FeatureStream(utility.get_date_file_path(feature_map_file), utility.get_date_file_path(train_data_file))
+    print "training......"
+    alg = lr.LR(train_fs.feature_count())
+    #alg.load_model(utility.get_date_file_path("app_model_1.txt"))
+    for i in xrange(iter):
+        train_fs.reset()
+        model_file = "app_model_lr_v3_{0}_{1}.json".format(eta, iter)
+        print "iter {0}......".format(i)
+        alg.train(train_fs, _lambda, eta, report_interval=-1)
+        alg.dump_model(utility.get_date_file_path(model_file))
+        print "testing......"
+        test_fs = utility.FeatureStream(utility.get_date_file_path(feature_map_file), utility.get_date_file_path(test_data_file))
+        alg.test(test_fs)
